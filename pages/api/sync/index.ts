806e738a6a9503a5ce07e506b35ba525133fd694
PabloMur <polillomurillo@gmail.com>
2023-08-06 18:20:22 -0300

    sync POST only

--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -2,7 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getOffsetLimit, getLista } from "lib/request";
 import { airtableBase } from "lib/airtable";
 import { productsIndex } from "lib/algolia";
-export default function (req: NextApiRequest, res: NextApiResponse) {
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).end("Method not allowed"); // Método no permitido
+  }
+
   const lista = getLista();
   const { limit, offset } = getOffsetLimit(req, 100, lista.length);
   const slicedList = lista.slice(offset, offset + limit);
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,12 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end("method not allowed"); // Método no permitido
+  }
+
   await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    methods: ["GET"],
     origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
   });
 
   res.send({ test: "test" });
